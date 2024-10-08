4944d2caea70e2f247784ee61aeab5194bb9a349
FootLettuce26 <caleb.weigt@cuw.edu>
2024-10-08 11:58:03 -0500

    started with the JSON file for time zones and value of change

--- regions.js
+++ regions.js
@@ -1,5 +1,6 @@
 import * as ReadTime from './home.js'; 
 import * as ReadDate from './dateCheck.js';
+import data from './regionsArray.json' with {type: 'json'};
 
 let timeChange; //describe the amount of hours that need to be changed in new region
 //current values of variables based on input
@@ -13,6 +14,12 @@ const year = ReadDate.ReadYear();
 
 //ayeeee this works
 export function ChangeTime(){
-    document.getElementById("NewYorkCityNY").innerText = "Hello World";
+   //possibly switching this to a for loop or something to check it
+   //assign variable the value based on the result of loop: data.regions[i]
+   const newYorkTime = data.regions[0]; 
+
+   //so far this prints the value of the time change
+   //next step is to figure out how to actually fix the time
+   document.getElementById("NewYorkCityNY").innerText = `${newYorkTime.zone}:  ${newYorkTime.timeChange}`;
 }
 
