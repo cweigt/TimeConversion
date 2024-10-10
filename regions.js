6f46f9752d6b56a20fbaea7f783093962bf61ecf
FootLettuce26 <caleb.weigt@cuw.edu>
2024-10-10 11:29:24 -0500

    Trying to get the stupid JSON file to work

--- regions.js
+++ regions.js
@@ -1,25 +1,24 @@
-import * as ReadTime from './home.js'; 
-import * as ReadDate from './dateCheck.js';
 import data from './regionsArray.json' with {type: 'json'};
 
-let timeChange; //describe the amount of hours that need to be changed in new region
-//current values of variables based on input
-const minute = ReadTime.ReadMinute(); 
-const hour = ReadTime.ReadHour();
-const meridiem = ReadTime.ReadMeridiem();
+export function ChangeTime() {
+   // Get the current time and date values
+   const minute = ReadTime.ReadMinute(); 
+   const hour = ReadTime.ReadHour();
+   const meridiem = ReadTime.ReadMeridiem();
 
-const month = ReadDate.ReadMonth();
-const day = ReadDate.ReadDay();
-const year = ReadDate.ReadYear();
+   const month = ReadDate.ReadMonth();
+   const day = ReadDate.ReadDay();
+   const year = ReadDate.ReadYear();
 
-//ayeeee this works
-export function ChangeTime(){
-   //possibly switching this to a for loop or something to check it
-   //assign variable the value based on the result of loop: data.regions[i]
-   const newYorkTime = data.regions[0]; 
 
-   //so far this prints the value of the time change
-   //next step is to figure out how to actually fix the time
-   document.getElementById("NewYorkCityNY").innerText = `${newYorkTime.zone}:  ${newYorkTime.timeChange}`;
+   // Loop through the regions data and update the time for each region
+   for (let tracker = 0; tracker < data.regions.length; tracker++) {
+      let time = data.regions[tracker];
+      let updatedHour = hour + time.timeChange;
+      
+      // Use region from JSON
+      let region = time.region; 
+      document.getElementById(region).innerText = `${time.zone}: ${updatedHour}`;
+      // Update the respective region's time
+   }
 }
-
